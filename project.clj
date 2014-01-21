dd45a236d7e7aafcc03752228d673b795d51b845
Allen Rohner <arohner@gmail.com>
2014-01-20 16:56:53 -0800

    Bump the version num

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject lein-version-spec "0.0.3"
-  :version-spec "0.1.~{:env/circle_build_num}-~{:env/circle_sha1}"
+(defproject lein-version-spec "0.0.4"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.incubator "0.1.1"]
