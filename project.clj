cfb49aeebf748c22a0396955c53c23e6b3d4db23
J Irving <j@lollyshouse.ca>
2015-02-19 14:40:07 -0500

    Move plugin into 'circleci' clojars group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-version-spec "0.0.5"
+(defproject circleci/lein-version-spec "0.0.5"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.incubator "0.1.1"]
